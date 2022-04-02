48336fa516f07f43694e35fa2cd3fde85ab43880
leonguyensang <im.nhsang@gmail.com>
2022-04-02 11:27:50 +0700

    test husky

--- src/database/index.js
+++ src/database/index.js
@@ -10,34 +10,34 @@ const connectToMongo = async () => {
     useUnifiedTopology: true,
   }
 
-  mongoose.connection.on('connecting', () => {
-    console.log('Connecting to Mongo...')
-  })
+  // mongoose.connection.on('connecting', () => {
+  //   console.log('Connecting to Mongo...')
+  // })
 
-  mongoose.connection.on('connected', () => {
-    console.log('Mongo is connected.')
-  })
+  // mongoose.connection.on('connected', () => {
+  //   console.log('Mongo is connected.')
+  // })
 
-  mongoose.connection.on('reconnected', () => {
-    console.log('Mongo trying to reconnect...')
-  })
+  // mongoose.connection.on('reconnected', () => {
+  //   console.log('Mongo trying to reconnect...')
+  // })
 
-  mongoose.connection.on('error', (error) => {
-    console.error('Unable to connect to the Mongo: ', error)
-  })
+  // mongoose.connection.on('error', (error) => {
+  //   console.error('Unable to connect to the Mongo: ', error)
+  // })
 
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongo has disconnected!')
+  // mongoose.connection.on('disconnected', () => {
+  //   console.log('Mongo has disconnected!')
 
-    // Trying to connect
-    const waitingMS = 5000
-    setTimeout(() => {
-      console.log(`Reconnecting in ${waitingMS / 1000}s...`)
-      return mongoose.connect(uri, options)
-    }, waitingMS)
-  })
+  //   // Trying to connect
+  //   const waitingMS = 5000
+  //   setTimeout(() => {
+  //     console.log(`Reconnecting in ${waitingMS / 1000}s...`)
+  //     return mongoose.connect(uri, options)
+  //   }, waitingMS)
+  // })
 
-  return mongoose.connect(uri, options)
+  // return mongoose.connect(uri, options)
 }
 
 export default { connectToMongo }
