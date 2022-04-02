58090f6b41e5027dba0e8039df57a3c4f49b600c
leonguyensang <im.nhsang@gmail.com>
2022-04-02 11:50:35 +0700

    test husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     semi         : ['error', 'never'],
     eqeqeq       : ['error', 'always'],
+    'no-console' : ['off'],
     'key-spacing': [
       2,
       {
